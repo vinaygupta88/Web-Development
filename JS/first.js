6cf08a694261cada3e393293029b3045a4d5e30f
Vinay Kumar <vinayku73094@gmail.com>
2026-05-28 15:45:50 +0530

    Update first.js

--- JS/first.js
+++ JS/first.js
@@ -1,1 +1,20 @@
-alert("This is first page alert")
+// alert("This is first page alert")
+/*console.log("First lecture")
+console.log("Wellcome to Js Lecture")*/
+// operators
+/* Day 2
+
+let a = 5;
+let b = 6;
+console.log("a+b= ",a+b);
+console.log("a-b= ",a-b);
+console.log("a*b= ",a*b);
+console.log("a/b= ",a/b);
+console.log("a**b= ",a**b); */
+
+let num = prompt("Enter a Number: ");
+if(num % 5 === 0){
+    console.log(num, "is multiple of 5")
+}else{
+    console.log(num,"is NOT multiple os 5")
+}
